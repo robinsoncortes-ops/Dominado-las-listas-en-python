b9248f070c697818afe38ad92e497f53e2627c6b
Robinson alejandro cortes salinas <robinsoncortes@colegioguanenta.edu.co>
2026-04-16 16:20:58 -0500

    Add files via upload

--- ejercicio 4/ejercicio 4.py
+++ ejercicio 4/ejercicio 4.py
@@ -1,2 +1,14 @@
 # funcion
 
+def numeros_positivos(lista):
+    contador = 0
+
+    for numero in lista:
+        if numero > 0:
+            contador +=1
+
+    return contador
+
+numeros = [1,3,4,45]
+resultado = numeros_positivos(numeros)
+print("cantidad de numeros positivos:", resultado)
